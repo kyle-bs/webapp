6fb3e6a36ad0b073eb679fc9860608650720bee3
kyle-bs <ksheldon@gmail.com>
2017-04-04 14:23:11 -0500

    Update app.py

--- webapp/app.py
+++ webapp/app.py
@@ -6,7 +6,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def hello():
-    provider = str(os.environ.get('PROVIDER', 'kyle break!'))
+    provider = str(os.environ.get('PROVIDER', 'world'))
     return 'Hello '+provider+'!'
 
 if __name__ == '__main__':
